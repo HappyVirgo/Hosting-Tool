5bb4958123b5cfbecbe7abf585b2ceeaa7316390
happyvirgo <happy0910virgo@outlook.com>
2020-12-30 21:21:40 +0700

    chore(eslint): Format eslint automatically for all tests

--- src/__tests__/ModalAddAccount.test.js
+++ src/__tests__/ModalAddAccount.test.js
@@ -1,91 +1,90 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-
+import {render, screen, fireEvent} from "@testing-library/react";
 import ModalAddAccount from "@/admin/components/ModalAddAccount";
 
 jest.mock("@/admin/providers/UserProvider", () => {
-  const UserContext = jest.requireActual("react").createContext({
-    user: {},
-    updateUser: jest.fn(),
-  });
+    const UserContext = jest.requireActual("react").createContext({
+        user: {},
+        updateUser: jest.fn()
+    });
 
-  return {
-    __esModule: true,
-    UserContext,
-    UserConsumer: UserContext.Consumer,
-  };
+    return {
+        __esModule: true,
+        UserContext,
+        UserConsumer: UserContext.Consumer
+    };
 });
 
-const setup = (overrides) => {
-  const props = {
-    onHide: jest.fn(),
+const setup = overrides => {
+    const props = {
+        onHide: jest.fn(),
 
-    ...overrides,
-  };
+        ...overrides
+    };
 
-  const wrapper = render(<ModalAddAccount {...props} />);
+    const wrapper = render(<ModalAddAccount {...props} />);
 
-  return {
-    wrapper,
-    props,
-  };
+    return {
+        wrapper,
+        props
+    };
 };
 
 describe("ModalAddAccount", () => {
-  [
-    ["Airbnb", "Link Airbnb Account"],
-    ["HomeAway", "Link VRBO Account"],
-    ["August", "Link August Account"],
-  ].forEach(([type, header]) => {
-    test(`should show modal link ${type}`, () => {
-      jest.useFakeTimers();
+    [
+        ["Airbnb", "Link Airbnb Account"],
+        ["HomeAway", "Link VRBO Account"],
+        ["August", "Link August Account"]
+    ].forEach(([type, header]) => {
+        test(`should show modal link ${type}`, () => {
+            jest.useFakeTimers();
 
-      setup({
-        show: true,
-        credentials: {
-          type,
-        },
-      });
+            setup({
+                show: true,
+                credentials: {
+                    type
+                }
+            });
 
-      jest.runAllTimers();
+            jest.runAllTimers();
 
-      expect(screen.queryByText(header)).toBeInTheDocument();
-    });
-  });
-
-  test("should show error without selecting a channel", () => {
-    setup({
-      show: true,
-      credentials: {
-        type: "",
-      },
+            expect(screen.queryByText(header)).toBeInTheDocument();
+        });
     });
 
-    fireEvent.click(screen.getByText("Next"));
+    test("should show error without selecting a channel", () => {
+        setup({
+            show: true,
+            credentials: {
+                type: ""
+            }
+        });
 
-    expect(screen.queryByText("Please select a channel.")).toBeInTheDocument();
-  });
+        fireEvent.click(screen.getByText("Next"));
 
-  [
-    ["airbnb-checkbox", "Link Airbnb Account"],
-    ["aribnb-logo-wrapper", "Link Airbnb Account"],
-    ["homeaway-checkbox", "Link VRBO Account"],
-    ["homeaway-logo-wrapper", "Link VRBO Account"],
-  ].forEach(([selector, result]) => {
-    test(`should display ${result} as click on ${
-      /checkbox$/.test(selector) ? "checkbox" : "wrapper"
-    }`, () => {
-      setup({
-        show: true,
-        credentials: {
-          type: "",
-        },
-      });
-
-      fireEvent.click(screen.queryByTestId(selector));
-      fireEvent.click(screen.queryByText("Next"));
-
-      expect(screen.queryByText(result)).toBeInTheDocument();
+        expect(screen.queryByText("Please select a channel.")).toBeInTheDocument();
     });
-  });
+
+    [
+        ["airbnb-checkbox", "Link Airbnb Account"],
+        ["aribnb-logo-wrapper", "Link Airbnb Account"],
+        ["homeaway-checkbox", "Link VRBO Account"],
+        ["homeaway-logo-wrapper", "Link VRBO Account"]
+    ].forEach(([selector, result]) => {
+        test(`should display ${result} as click on ${
+            /checkbox$/.test(selector) ? "checkbox" : "wrapper"
+        }`, () => {
+            setup({
+                show: true,
+                credentials: {
+                    type: ""
+                }
+            });
+
+            fireEvent.click(screen.queryByTestId(selector));
+            fireEvent.click(screen.queryByText("Next"));
+
+            expect(screen.queryByText(result)).toBeInTheDocument();
+        });
+    });
 });
