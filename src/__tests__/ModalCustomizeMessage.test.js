615802f1a3aa360ca56d7e548d590e558bfe9b1e
happyvirgo <happy0910virgo@outlook.com>
2020-12-31 14:53:59 +0700

    feat(ut): Add tests for 3 modal components

---
+++ src/__tests__/ModalCustomizeMessage.test.js
@@ -0,0 +1,169 @@
+import React from "react";
+import {render, screen, fireEvent, waitForElementToBeRemoved} from "@testing-library/react";
+import {MemoryRouter} from "react-router-dom";
+import ModalCustomizeMessage from "@/admin/components/ModalCustomizeMessage";
+
+jest.mock("@/admin/providers/UserProvider", () => {
+    const UserContext = jest.requireActual("react").createContext({
+        user: {
+            tags: [
+                {
+                    name: "baz"
+                }
+            ]
+        },
+        updateUser: jest.fn()
+    });
+
+    return {
+        __esModule: true,
+        UserContext,
+        UserConsumer: UserContext.Consumer
+    };
+});
+
+jest.mock("@/admin/components/TextareaWithTags", () => ({
+    __esModule: true,
+    default: props => (
+        <>
+            <label>{props.messages.default}</label>
+            <input
+                data-testid="input-message"
+                aria-label="Input message"
+                type="text"
+                value=""
+                onChange={e => {
+                    props.onChange(e.target.value);
+                }}
+            />
+        </>
+    )
+}));
+
+const setup = overrides => {
+    const props = {
+        show: false,
+        messageRuleMessage: "my message",
+        onHide: jest.fn(),
+
+        ...overrides
+    };
+
+    const wrapper = render(
+        <MemoryRouter>
+            <ModalCustomizeMessage {...props} />
+        </MemoryRouter>
+    );
+
+    return {
+        wrapper,
+        props
+    };
+};
+
+describe("ModalCustomizeMessage", () => {
+    test("should open the modal", () => {
+        const {wrapper, props} = setup();
+
+        wrapper.rerender(
+            <MemoryRouter>
+                <ModalCustomizeMessage {...props} show />
+            </MemoryRouter>
+        );
+
+        expect(screen.queryByText("Customize Message")).toBeInTheDocument();
+    });
+
+    test("should change the message successfully", () => {
+        const {wrapper, props} = setup();
+
+        wrapper.rerender(
+            <MemoryRouter>
+                <ModalCustomizeMessage {...props} show />
+            </MemoryRouter>
+        );
+
+        const newMessage = "new message";
+        fireEvent.change(screen.queryByLabelText("Input message"), {
+            target: {value: newMessage}
+        });
+
+        expect(screen.queryByText(newMessage)).toBeInTheDocument();
+    });
+
+    test("should submit the message successfully", async () => {
+        const {wrapper, props} = setup();
+
+        wrapper.rerender(
+            <MemoryRouter>
+                <ModalCustomizeMessage {...props} show />
+            </MemoryRouter>
+        );
+
+        global.fetch = jest.fn().mockResolvedValue({
+            ok: true,
+            json: jest.fn().mockResolvedValue({})
+        });
+
+        fireEvent.click(screen.queryByText("Save"));
+
+        const spinner = screen.queryByTestId("spinner");
+        expect(spinner).toBeInTheDocument();
+
+        await waitForElementToBeRemoved(spinner);
+        expect(props.onHide).toHaveBeenCalled();
+
+        expect(global.fetch).toHaveBeenCalledWith(
+            "/customizeMessage",
+            expect.objectContaining({
+                body: JSON.stringify({
+                    message: props.messageRuleMessage,
+                    messageRuleMessage: props.messageRuleMessage,
+                    messageRuleID: "",
+                    airbnbUserID: "",
+                    airbnbListingID: "",
+                    airbnbConfirmationCode: ""
+                }),
+                method: "POST"
+            })
+        );
+    });
+
+    test("should fail to submit the message", async () => {
+        const {wrapper, props} = setup();
+
+        wrapper.rerender(
+            <MemoryRouter>
+                <ModalCustomizeMessage {...props} show />
+            </MemoryRouter>
+        );
+
+        global.fetch = jest.fn().mockRejectedValue("500");
+
+        fireEvent.click(screen.queryByText("Save"));
+
+        const spinner = screen.queryByTestId("spinner");
+        expect(spinner).toBeInTheDocument();
+
+        await waitForElementToBeRemoved(spinner);
+    });
+
+    test("should reset things", async () => {
+        const {wrapper, props} = setup();
+
+        wrapper.rerender(
+            <MemoryRouter>
+                <ModalCustomizeMessage {...props} show />
+            </MemoryRouter>
+        );
+
+        const newMessage = "new message";
+        fireEvent.change(screen.queryByLabelText("Input message"), {
+            target: {value: newMessage}
+        });
+
+        fireEvent.click(screen.queryByText("Reset"));
+
+        expect(screen.queryByText(props.messageRuleMessage)).toBeInTheDocument();
+    });
+});
