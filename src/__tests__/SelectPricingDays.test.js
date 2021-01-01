6b60be7208953f9948094d1bbb6a5ea7b14ff13b
happyvirgo <happy0910virgo@outlook.com>
2021-01-01 20:35:13 +0700

    implement unit test to SelectLimitDays, SelectLimitEvent, SelectListing, SelectLock, SelectMessageTags, SelectPriceSource, SelectPricingDays

---
+++ src/__tests__/SelectPricingDays.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {render, screen, fireEvent, waitFor} from "@testing-library/react";
+import SelectPricingDays from "../admin/components/SelectPricingDays";
+
+const setup = bindings => {
+    const props = {
+        selectedValue: "weekdays",
+        onSelectedOption: jest.fn(),
+        ...bindings
+    };
+
+    const view = render(<SelectPricingDays {...props} />);
+
+    return {
+        view,
+        props
+    };
+};
+
+test("should change option when selected value change", () => {
+    const {view, props} = setup();
+    const {rerender} = view;
+    const {queryByText} = screen;
+
+    expect(queryByText("Only Weekdays")).toBeInTheDocument();
+
+    rerender(<SelectPricingDays {...props} selectedValue="all" />);
+    expect(queryByText("All Days")).toBeInTheDocument();
+});
+
+test("should call `onSelectedOption` props when select new option", async () => {
+    const {view, props} = setup();
+    const {container} = view;
+    const {queryByText} = screen;
+
+    fireEvent.keyDown(container.firstChild, {key: "ArrowDown"});
+    await waitFor(() => queryByText("Only Weekends"));
+    fireEvent.click(queryByText("Only Weekends"));
+
+    expect(queryByText("Only Weekends")).toBeInTheDocument();
+    expect(props.onSelectedOption).toHaveBeenCalledWith({
+        label: "Only Weekends",
+        value: "weekends"
+    });
+});
