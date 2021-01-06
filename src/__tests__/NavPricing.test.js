012de308d699b133b50a8dce81fc05e50ad42ca5
kirannasim <kiran.cse7@gmail.com>
2021-01-06 04:27:08 -0800

    eslint code

--- src/__tests__/NavPricing.test.js
+++ src/__tests__/NavPricing.test.js
@@ -1,8 +1,7 @@
 import React from "react";
-import {render, screen, fireEvent, waitFor, within} from "@testing-library/react";
+import {render, screen, fireEvent, waitFor} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import NavPricing from "@/admin/components/NavPricing";
-
 
 const setup = overrides => {
     const props = {
@@ -20,66 +19,72 @@ const setup = overrides => {
                     airbnbName: "Happy 2 Airbnb",
                     nickName: "happy2",
                     airbnbUserID: "2",
-                    airbnbListingID: "234",
+                    airbnbListingID: "234"
                 }
             ],
             ...overrides
         }
     };
 
-    const wrapper = render(<BrowserRouter><NavPricing {...props} /></BrowserRouter>);
+    const wrapper = render(
+        <BrowserRouter>
+            <NavPricing {...props} />
+        </BrowserRouter>
+    );
 
     return {
         wrapper,
         props
     };
-}
+};
 
 const doClickShow = () => {
     fireEvent.click(screen.getByText("Pricing"));
 };
 
 describe("NavPricing", () => {
-    test("should render if there are several listings", async() => {
+    test("should render if there are several listings", async () => {
         setup();
         doClickShow();
-        await waitFor(() => screen.getByText('Happy 1 Airbnb'));
-        await waitFor(() => screen.getByText('Happy 2 Airbnb'));
-        expect(screen.getByText('Happy 1 Airbnb')).toBeInTheDocument();
-        expect(screen.getByText('Happy 2 Airbnb')).toBeInTheDocument();
+        await waitFor(() => screen.getByText("Happy 1 Airbnb"));
+        await waitFor(() => screen.getByText("Happy 2 Airbnb"));
+        expect(screen.getByText("Happy 1 Airbnb")).toBeInTheDocument();
+        expect(screen.getByText("Happy 2 Airbnb")).toBeInTheDocument();
     });
-    test("should render if there is only one listing", async() => {
-        setup(
-            {
-                listings: [
-                    {
-                        listingEnabled: true,
-                        airbnbName: "Happy 3 Airbnb",
-                        nickName: "happy3",
-                        airbnbUserID: "3",
-                        airbnbListingID: "134",
-                    }
-                ]
-            }
-        );
-        expect(screen.getByText('Pricing')).toBeInTheDocument();
-        expect(screen.getByRole('link', {
-            name: "Pricing"
-        })).toHaveAttribute("href", "/pricing/3/134");
+    test("should render if there is only one listing", async () => {
+        setup({
+            listings: [
+                {
+                    listingEnabled: true,
+                    airbnbName: "Happy 3 Airbnb",
+                    nickName: "happy3",
+                    airbnbUserID: "3",
+                    airbnbListingID: "134"
+                }
+            ]
+        });
+        expect(screen.getByText("Pricing")).toBeInTheDocument();
+        expect(
+            screen.getByRole("link", {
+                name: "Pricing"
+            })
+        ).toHaveAttribute("href", "/pricing/3/134");
     });
     const links = [
-        { text: 'Happy 1 Airbnb', location: "/pricing/1/123" },
-        { text: 'Happy 2 Airbnb', location: "/pricing/2/234" }
-      ];
-    test.each(links)("Check if Nav Bar have %s link.", async(link)=> {
+        {text: "Happy 1 Airbnb", location: "/pricing/1/123"},
+        {text: "Happy 2 Airbnb", location: "/pricing/2/234"}
+    ];
+    test.each(links)("Check if Nav Bar have %s link.", async link => {
         setup();
         doClickShow();
-        await waitFor(() => screen.getByRole('link', {
+        await waitFor(() =>
+            screen.getByRole("link", {
+                name: link.text
+            })
+        );
+        const linkDom = screen.getByRole("link", {
             name: link.text
-        }));
-        const linkDom = screen.getByRole('link', {
-            name: link.text
-        })
+        });
         expect(linkDom).toHaveAttribute("href", link.location);
     });
 });
