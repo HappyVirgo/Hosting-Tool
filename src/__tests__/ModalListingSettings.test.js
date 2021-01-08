36cd1294b6f3bf4e43bd56cfbd106b325dbbacf2
Phan Van Duong <duong.phan@go1.com>
2021-01-08 08:42:42 +0700

    implement unit test to ModalListingSettings 81% coverage

--- src/__tests__/ModalListingSettings.test.js
+++ src/__tests__/ModalListingSettings.test.js
@@ -206,7 +206,9 @@ describe("linkedListings", () => {
     test('should add linked listing when select listing options', async () => {
         setupLinkedListings();
 
-        fireEvent.keyDown(screen.queryByText("Listings...").parentElement.parentElement.parentElement, {key: "ArrowDown"});
+        const listingSelect = screen.queryByText("Listings...").parentElement.parentElement.parentElement;
+
+        fireEvent.keyDown(listingSelect, {key: "ArrowDown"});
 
         await waitFor(() => screen.queryByText("airbnbName_04"));
         fireEvent.click(screen.queryByText("airbnbName_04"));
@@ -214,14 +216,202 @@ describe("linkedListings", () => {
         expect(screen.queryByText("airbnbName_04")).toBeInTheDocument();
     });
 
-    test('should remove linked listing when select listing options', async () => {
+    test('should remove linked listing', async () => {
         setupLinkedListings();
 
-        const removeButton = screen.queryByText("nickname_01").parentElement.parentElement.querySelector("button");
-        fireEvent.click(removeButton);
+        const removeLikedLinkButton = screen.queryByText("nickname_01").parentElement.parentElement.querySelector("button");
+        fireEvent.click(removeLikedLinkButton);
 
         expect(screen.queryByText("nickname_01")).not.toBeInTheDocument();
         expect(screen.queryByText("airbnbName_03")).toBeInTheDocument();
     });
 
 });
+
+describe('linkedLocks', () => {
+    const setupLockedListings = () => {
+        const user = {
+            accounts: [{}],
+            badAccounts: [{}],
+            locks: [{
+                _id: "lock_id_01",
+                listingID: "id_123",
+                source: "source_lock_01",
+                name: "name_01",
+            },
+            {
+                _id: "lock_id_02",
+                source: "source_lock_02",
+                name: "name_02",
+            },
+            {
+                _id: "lock_id_03",
+                listingID: "id_123",
+                source: "source_lock_03",
+                name: "name_03",
+            },
+            {
+                _id: "lock_id_04",
+                source: "source_lock_04",
+                name: "name_04",
+            }],
+            listings: [{}],
+            listingGroups: [{}],
+            tags: [{ name: "Tag 1" }, { name: "Tag 2" }, { name: "Tag 3" }],
+        }
+        const params = setup({
+            show: true,
+        }, {
+            user,
+        });
+
+        return params;
+    }
+
+    test("should build linked lock and lock options", () => {
+        setupLockedListings();
+
+        expect(screen.queryByText("name_01")).toBeInTheDocument();
+        expect(screen.queryByText("name_03")).toBeInTheDocument();
+    });
+
+    test('should add linked lock when select listing options', async () => {
+        setupLockedListings();
+
+        const lockOptionSelect = screen.queryByText("Locks...").parentElement.parentElement.parentElement;
+
+        fireEvent.keyDown(lockOptionSelect, {key: "ArrowDown"});
+
+        await waitFor(() => screen.queryByText("name_02"));
+        fireEvent.click(screen.queryByText("name_02"));
+
+        expect(screen.queryByText("name_02")).toBeInTheDocument();
+    });
+
+    test('should remove linked lock', async () => {
+        setupLockedListings();
+
+        const removeLinkedLockButton = screen.queryByText("name_01").parentElement.parentElement.querySelector("button");
+        fireEvent.click(removeLinkedLockButton);
+
+        expect(screen.queryByText("name_01")).not.toBeInTheDocument();
+        expect(screen.queryByText("name_03")).toBeInTheDocument();
+    });
+});
+
+describe('isLinkedListing', () => {
+    const setupLinkedListing = (listing) => {
+
+        const user = {
+            accounts: [{}],
+            badAccounts: [{}],
+            listings: [{
+                _id: "listing_id_01",
+                source: "source_listing_01",
+                airbnbName: "airbnbName_01",
+                ...listing,
+            }],
+            locks: [{}],
+            listingGroups: [{}],
+            tags: [{ name: "Tag 1" }, { name: "Tag 2" }, { name: "Tag 3" }],
+        }
+
+        const params = setup({
+            listing: {
+                _id: "id_123",
+                airbnbName: "airbnbName_123",
+                airbnbUserID: "airbnbUserID_123",
+                airbnbListingID: "airbnbListingID_123",
+                nickname: "Nickname",
+                calendarExportCode: "calendarExportCode_123",
+                linkedListingID: "listing_id_01",
+                listingEnabled: true,
+                source: "source_listing_123",
+            },
+            show: true,
+        }, {
+            user,
+        });
+
+        return params;
+    }
+
+    test('should show airbnbName in listing props', () => {
+        setupLinkedListing();
+
+        expect(screen.queryByText("airbnbName_01")).toBeInTheDocument();
+    });
+
+    test('should show nickname in listing props', () => {
+        setupLinkedListing({
+            nickname: "nickname_123",
+        });
+
+        expect(screen.queryByText("nickname_123")).toBeInTheDocument();
+    });
+});
+
+describe('selectPriceSource', () => {
+    test('should change price source', async () => {
+        const {view: {baseElement}} = setup({
+            show: true
+        });
+
+        const priceSourceSelectElement = screen.queryByText("Set Amount").parentElement.parentElement.parentElement;
+        fireEvent.keyDown(priceSourceSelectElement, {key: "ArrowDown"});
+        await waitFor(() => screen.queryByText("Airbnb Smart Prices"));
+        fireEvent.click(screen.queryByText("Airbnb Smart Prices"));
+
+        expect(screen.queryByText("Airbnb Smart Prices")).toBeInTheDocument();
+    });
+});
+
+jest.mock("crypto", () => ({
+    randomBytes: () => Promise.resolve("byte_01"),
+}));
+
+describe('calendarExportCode', () => {
+    const setupCalendarExportCode = (calendarExportCode = "") => {
+        const params = setup({
+            show: true,
+            listing: {
+                _id: "id_123",
+                airbnbName: "bnb_123",
+                airbnbUserID: "airbnbUserID_123",
+                airbnbListingID: "airbnbListingID_123",
+                nickname: "Nickname",
+                calendarExportCode,
+                linkedListingID: null,
+                listingEnabled: true,
+                source: "source_listing_123",
+            },
+        });
+        return params;
+    };
+
+    test('should generate new calendar export code when toggle on Export Calendar', async () => {
+
+        setupCalendarExportCode();
+
+        fireEvent.click(screen.getByRole("checkbox", {name: "Export Calendar"}));
+
+        await Promise.resolve();
+
+        expect(screen.getByDisplayValue("https://hosttools.com/ical/byte_01")).toBeInTheDocument();
+    });
+
+    test('should generate new calendar export code when click button generate new url', async () => {
+
+        setupCalendarExportCode("exportCalendarCode_123");
+
+        expect(screen.getByDisplayValue("https://hosttools.com/ical/exportCalendarCode_123")).toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole("button", {name: "New URL"}));
+
+        await Promise.resolve();
+
+        expect(screen.getByDisplayValue("https://hosttools.com/ical/byte_01")).toBeInTheDocument();
+    });
+
+});
+
