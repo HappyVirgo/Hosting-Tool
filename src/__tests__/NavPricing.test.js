a7cdc70bdce7896379701af79f44dabc405f3fd9
happyvirgo <happy0910virgo@outlook.com>
2021-01-07 22:11:26 -0800

    eslint code

--- src/__tests__/NavPricing.test.js
+++ src/__tests__/NavPricing.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {render, screen, fireEvent, waitFor} from "@testing-library/react";
-// import {act} from "@testing-library/react-hooks"
 import {BrowserRouter} from "react-router-dom";
 import NavPricing from "@/admin/components/NavPricing";
 
@@ -140,17 +139,17 @@ describe("NavPricing", () => {
         });
         expect(linkDom).toHaveAttribute("href", link.location);
     });
-    test("check if search filter works well", async() => {
+    test("check if search filter works well", async () => {
         const {props} = setup();
         doClickShow();
         await waitFor(() => screen.getByText(/listings/i));
-        expect(screen.getByRole('searchbox')).toBeInTheDocument();
-        const input = screen.getByPlaceholderText('Filter...')
+        expect(screen.getByRole("searchbox")).toBeInTheDocument();
+        const input = screen.getByPlaceholderText("Filter...");
         fireEvent.change(input, {target: {value: "happy1"}});
         expect(input.value).toBe("happy1");
-        const listings = props.user.listings.filter((listing) => listing.nickname===input.value);
+        const listings = props.user.listings.filter(listing => listing.nickname === input.value);
         setup({
-            listings: listings
+            listings
         });
         expect(
             screen.getByRole("link", {
